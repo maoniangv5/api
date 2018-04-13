b26d3486e4efe5f596b7fc99602ff6c367d7b5f4
chenpeng <846869169@qq.com>
2018-04-13 17:56:39 +0800

    db

--- db/db.js
+++ db/db.js
@@ -1,7 +1,7 @@
 let mongoose = require('mongoose');
 let config = require('../config/config');
 
-let url = `mongodb://${config.db.user}:${config.db.pwd}@${config.db.host}/${config.db.port}/${config.db.db}`
+let url = `mongodb://${config.db.user}:${config.db.pwd}@${config.db.host}:${config.db.port}/${config.db.db}`
 
 mongoose.connect(url)
 
